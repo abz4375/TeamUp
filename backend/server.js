7d00ef40ddfe06c2afee4264e10898f4f0772037
abz4375 <abz4375.ayushsrivastava@gmail.com>
2023-08-05 00:17:38 +0530

    initialised project

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const colors = require('colors')
+const connectDB = require('./config/db')
+const port = process.env.PORT || 5000
+
+connectDB()
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
+
+app.listen( port, () => console.log(`Server started on port ${port}`))
