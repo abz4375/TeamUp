aceec8e631569d6f5b67ac277a2e74953fa2b424
abz4375 <abz4375.ayushsrivastava@gmail.com>
2024-01-31 19:11:17 +0530

    fresh-start

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')  
-const router = express.Router()
-const {getUsers,setUser,deleteUser} = require('../controllers/userController')
-
-router.route('/').get(getUsers).post(setUser)
-router.route('/:id').delete(deleteUser)
-
-
-module.exports = router 
